136e0117e215a6fc3a7987cae6e75aa70d7afd2b
Fredy Saul Teheran Tovar <fredyteheran91@gmail.com>
2017-07-05 05:23:29 -0500

    Edit v mobile tablt

--- assets/js/forestahotellima.js
+++ assets/js/forestahotellima.js
@@ -23,4 +23,46 @@ $(document).ready(function() {
       opacity: '0'
     }, 'slow');
   });
+  $('.datepicker').pickadate({
+    monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+    today: 'Hoy',
+    clear: 'Borrar',
+    format: 'dd, mmm, yyyy',
+    formatSubmit: 'yyyy/mm/dd',
+    selectMonths: true,
+    selectYears: true
+  });
+  $('.select').selectric();
+  $('.buscar').on('click', function() {
+    var llega = $('#hllegada').val();
+    var sale = $('#hsalida').val();
+    var adult = $('#adulto').val();
+    var nino = $('#nino').val();
+    var Dia = llega.substring(0, 2);
+    var Mes = llega.substring(4, 7);
+    var Ano = llega.substring(9, 14);
+    var sale = $('#hsalida').val();
+    var Dias = sale.substring(0, 2);
+    var Mess = sale.substring(4, 7);
+    var Anos = sale.substring(9, 14);
+    console.log(Dia + '----' + Mes + '----' + Ano);
+    $('.resultado').show().animate({
+      height: '340px',
+      opacity: '1'
+    }, 'slow');
+    $('.reservas').hide().animate({
+      opacity: '0'
+    }, 'slow');
+    $('.fcdia').append(Dia);
+    $('.fcmes').append(Mes);
+    $('.fcano').append(Ano);
+    // 
+    $('.fcdias').append(Dias);
+    $('.fcmess').append(Mess);
+    $('.fcanos').append(Anos);
+    // 
+    $('.fcadult').append(adult);
+    $('.fcnino').append(nino);
+  });
 });
